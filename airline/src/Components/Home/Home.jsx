d32069506bd0f3c4b1f8b8e2624fe38b47e3d01b
buynaaaa24 <156633434+buynaaaa24@users.noreply.github.com>
2024-05-15 19:01:04 +0800

    home zasah gj uzse ajillulad uzere mnih deer dev n ajildgue

--- airline/src/Components/Home/Home.jsx
+++ airline/src/Components/Home/Home.jsx
@@ -4,16 +4,22 @@ import React, {useEffect} from 'react'
 import video from '../../assets/video.mp4'
 import aeroplane from '../../assets/takeOff.png'
 
-//Import AOS
+//Import AOS ===========>
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 const Home = () => {
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  }, [])
+
+
   return (
     <div className='home flex container'>
       
       <div className="mainText">
-        <h1>Create Ever-lasting Memories With Us</h1>
+        <h1 data-aos='fade-up' data-aos-duration='2500'> Create Ever-lasting Memories With Us</h1>
       </div>
+      <div data-aos='fade-down' data-aos-duration='2500' className='homeImage flex'></div>
 
       <div className="homeImages flex">
         
